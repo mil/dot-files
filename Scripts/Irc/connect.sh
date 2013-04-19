3040c090c3e591c8834d4bdd6046b71f3b1fb49c
mil <miles.sandlar@gmail.com>
2013-04-19 14:09:47 -0400

    macbook work with configs & add iii scripts

---
+++ Scripts/Irc/connect.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env sh
+
+: "${ircdir:=$HOME/irc}"
+: "${nick:=$USER}"
+
+# server info functions
+freenode() {
+	server='irc.freenode.net'
+	channels="#weeklish_hack #hackerschool #io #regex"
+}
+
+oftc() {
+	server='irc.oftc.net'
+	channels="#vtluug #suckless"
+}
+
+# these match the functions above
+networks="freenode oftc"
+
+# some privacy please, thanks
+chmod 700 "$ircdir"
+chmod 600 "$ircdir"/*/ident &>/dev/null
+
+for network in $networks; do
+	unset server channels port
+	"$network" # set the appropriate vars
+
+	while true; do
+		# cleanup
+		rm -f "$ircdir/$server/in"
+
+		# connect to netwrok -- password is set through the env var synonym to the network name
+		iim -i "$ircdir" -n "$nick" -k "$network" -s "$server" -p "${port:-6667}" &
+		pid="$!"
+
+		# wait for the connection
+		while ! test -p "$ircdir/$server/in"; do sleep .3; done
+
+		# auth to services
+		if [ -e "$ircdir/$server/ident" ]
+		then printf "/j nickserv identify %s\n" "$(cat "$ircdir/$server/ident")" > "$ircdir/$server/in"
+		fi && rm -f "$ircdir/$server/nickserv/out" # clean that up - ident passwd is in there
+
+		# join channels
+		printf "/j %s\n" $channels > "$ircdir/$server/in"
+
+		# if connection is lost reconnect
+		wait "$pid"
+	done &
+done
+
