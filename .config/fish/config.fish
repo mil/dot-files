cffe119a108a1af58d265cee1a58134e178e6cf0
mil <miles.sandlar@gmail.com>
2015-03-21 13:18:46 -0400

    fish right align

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,12 +51,31 @@ setenv TZ America/New_York
 set fish_greeting ""
 
 
+# fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '☡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+function fish_right_prompt
+  printf '%s' (__fish_git_prompt) 
+end 
 function fish_prompt
+  set last_status $status
+  set_color $fish_color_cwd
+  printf '%s→ ' (prompt_pwd)
+  set_color normal
+  set_color normal
   z --add $PWD
   set_color $fish_color_cwd
-  echo -n (prompt_pwd)
   set_color normal
-  echo -n ' > '
 end
 
 cat ~/.todo
