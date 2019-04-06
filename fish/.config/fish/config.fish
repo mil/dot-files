52a281939b6f434fdc0e0b2b318f0d32c394548c
mil <m@userbound.com>
2019-04-06 12:40:49 -0500

    Misc

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,8 +15,7 @@ function setup_envvars_and_path
   #setenv PAGER 'w3m -X'
   setenv PAGER w3m
   setenv TZ America/Chicago
-  set -x SURFRAW_graphical false
-  set -x LEDGER_FILE ~/.ledger.journal
+  setenv SURFRAW_graphical false
 
   # Path
   add_to_path_if_exists /home/$USER/.cargo/bin
@@ -105,7 +104,7 @@ function setup_vimlike
         case insert;  set_color --bold --background green white
         case visual;  set_color --bold --background magenta white
     end
-    echo ' '
+    date '+%H%M'
     set_color normal
     echo -n ' '
   end
